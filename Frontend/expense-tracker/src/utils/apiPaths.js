8492486d5376aa1a9a4ca76c6d355e35fff5a9af
VikasYadav-1 <vikas_12015030@nitkkr.ac.in>
2025-11-01 22:13:18 +0530

    updated apiPath.js

--- Frontend/expense-tracker/src/utils/apiPaths.js
+++ Frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,4 @@
-export const BASE_URL='https://expense-tracker-backend-beta-two.vercel.app/';
+export const BASE_URL='https://expense-tracker-frontend-two-woad.vercel.app/';
 
 export const API_PATHS = {
     AUTH: {
